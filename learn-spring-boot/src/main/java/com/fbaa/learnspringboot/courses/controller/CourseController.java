da89837e4d9e56f0dad9df2fd678c7f8ba7c16a4
frederico alexandre <fred.bralvalex@gmail.com>
2022-09-27 19:59:53 -0400

    First rest service with spring boot

---
+++ learn-spring-boot/src/main/java/com/fbaa/learnspringboot/courses/controller/CourseController.java
@@ -0,0 +1,18 @@
+package com.fbaa.learnspringboot.courses.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import bean.Course;
+
+@RestController
+public class CourseController {
+	
+	@GetMapping("/courses")
+	public List<Course> getAllCourses() {
+		return List.of(new Course(1, "Learn Microservices", "in28minutes"));
+	}
+
+}
