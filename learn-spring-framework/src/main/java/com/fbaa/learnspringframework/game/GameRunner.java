0bea3f0cc5ddd02143dd090c0df6957eb062865f
frederico alexandre <fred.bralvalex@gmail.com>
2022-09-27 16:35:29 -0400

    Injection options

--- learn-spring-framework/src/main/java/com/fbaa/learnspringframework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/fbaa/learnspringframework/game/GameRunner.java
@@ -13,11 +13,18 @@ public class GameRunner {
 		this.game = game;
 	}
 	
+//  set injection	
+//	@Autowired
+//	public void setGame(GameConsole game) {
+//		this.game = game;
+//	}
+	
 	public void runGame() {
 		game.up();
 		game.down();		
 		game.left();
 		game.right();
 	}
+
 	
 }
