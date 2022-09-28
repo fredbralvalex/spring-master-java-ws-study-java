4669271103f98f6889c11c5170072053d4aaa5eb
frederico alexandre <fred.bralvalex@gmail.com>
2022-09-28 14:58:29 -0400

    adding Rest API
    hello world

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring-master-java-ws-study-java
+
+
+https://github.com/in28minutes/spring-web-services/tree/master/soap-web-services
---
+++ restful-web-services/src/main/java/com/fbaa/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.fbaa.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/hello-world")
+//	@RequestMapping(method = RequestMethod.GET, path="/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World!");
+	}
+
+}
