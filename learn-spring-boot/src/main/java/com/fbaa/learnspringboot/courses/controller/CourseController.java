620403c7d7b9163d4e96ac69083edb4ee6bf71a6
frederico alexandre <fred.bralvalex@gmail.com>
2022-09-28 10:21:00 -0400

    actuator and devtools

--- learn-spring-boot/src/main/java/com/fbaa/learnspringboot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/fbaa/learnspringboot/courses/controller/CourseController.java
@@ -3,16 +3,25 @@ package com.fbaa.learnspringboot.courses.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bean.Course;
 
+@RequestMapping("/")
 @RestController
 public class CourseController {
 	
+	List<Course> list =  List.of(new Course(1, "Learn Microservices I", "in28minutes"), new Course(2, "Learn FS with Angular and React", "in28minutes"));
 	@GetMapping("/courses")
 	public List<Course> getAllCourses() {
-		return List.of(new Course(1, "Learn Microservices", "in28minutes"));
+		return list;
+	}
+	
+	@GetMapping("/courses/{courseId}")
+	public Course getCourseDetails (@PathVariable long courseId) {
+		return list.stream().filter(course -> course.getId() == courseId).findFirst().get();
 	}
 
 }
