1fb919a4b78ceff094d06fdd7df3332eb1583006
frederico alexandre <fred.bralvalex@gmail.com>
2022-09-28 15:12:56 -0400

    Hello world updates

--- restful-web-services/src/main/java/com/fbaa/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ restful-web-services/src/main/java/com/fbaa/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -22,6 +22,4 @@ public class HelloWorldBean {
 		return "HelloWorldBean [message=" + message + "]";
 	}
 
-	
-
 }
--- restful-web-services/src/main/java/com/fbaa/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/fbaa/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.fbaa.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +18,9 @@ public class HelloWorldController {
 		return new HelloWorldBean("Hello World!");
 	}
 
+	@GetMapping("/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
 }
