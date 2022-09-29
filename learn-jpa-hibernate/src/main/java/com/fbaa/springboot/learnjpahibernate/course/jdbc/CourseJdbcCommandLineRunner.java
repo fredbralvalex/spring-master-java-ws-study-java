6b75f02f0e3796e0127d64e7639d23e724cbe152
frederico alexandre <fred.bralvalex@gmail.com>
2022-09-29 16:43:01 -0400

    dynamic query

--- learn-jpa-hibernate/src/main/java/com/fbaa/springboot/learnjpahibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-hibernate/src/main/java/com/fbaa/springboot/learnjpahibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.fbaa.springboot.learnjpahibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
@@ -12,7 +14,14 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert();
+		Course course = new Course(1, "Learn AWS Now", "In 28 Minutes Now");
+		Course course2 = new Course(2, "Learn Azure Now", "In 28 Minutes Now");
+		Course course3 = new Course(3, "Learn Devops Now", "In 28 Minutes Now");
+		repository.insert(course);
+		repository.insert(course2);
+		repository.insert(course3);
+		
+		repository.delete(2);
 		
 	}
 
--- learn-jpa-hibernate/src/main/java/com/fbaa/springboot/learnjpahibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-hibernate/src/main/java/com/fbaa/springboot/learnjpahibernate/course/jdbc/CourseJdbcRepository.java
@@ -4,20 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.fbaa.springboot.learnjpahibernate.course.Course;
+
 @Repository
 public class CourseJdbcRepository {
 
 	@Autowired
 	private JdbcTemplate springJdbcTemplate;
 	
-	private static String INSERT_QUERY = 
+	private static final String INSERT_QUERY = 
 			"""
 		
-					insert into course (id, name, author) values (1, 'Learn AWS', 'In 28 Minutes');
+					insert into course (id, name, author) values (?, ?, ?);
 	
 			""";
 	
-	public void insert() {
-		springJdbcTemplate.update(INSERT_QUERY);
+	private static final String DELETE_QUERY = 
+			"""
+		
+					delete from course where id=?;
+	
+			""";
+	
+	public void insert(Course course) {
+		springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+	}
+
+	public void delete(long courseId) {
+		springJdbcTemplate.update(DELETE_QUERY, courseId);
 	}
 }
