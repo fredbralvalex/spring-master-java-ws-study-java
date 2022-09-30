dd795ec5743be655a2702b016f95684652ec57f4
frederico alexandre <fred.bralvalex@gmail.com>
2022-09-30 14:23:44 -0400

    Spring security

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- only works for embbeded database
 insert into user_details (id, birth_date, name) values (10001, current_date(), 'Fred');
 insert into user_details (id, birth_date, name) values (10002, current_date(), 'Robbie');
 insert into user_details (id, birth_date, name) values (10003, current_date(), 'Jem');
